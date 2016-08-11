630cfe2a2aca164be6a2848cee2b0be8df1cc140
Anand Thakker <vestibule@anandthakker.net>
2016-08-10 22:15:42 -0400

    Allow multiple addSourceType calls with same name
    Since `addSourceType` adds the source type to a static map, it was
    yielding an error if two map instances tried to add the same source
    type.
    
    This changes `addSourceType(name, SourceType)` to simply do nothing if
    there is already a source type named `name`.

--- js/style/style.js
+++ js/style/style.js
@@ -672,7 +672,7 @@ Style.prototype = util.inherit(Evented, {
 
     addSourceType: function (name, SourceType, callback) {
         if (Source.getType(name)) {
-            return callback(new Error('A source type called "' + name + '" already exists.'));
+            return callback(null, null);
         }
 
         Source.setType(name, SourceType);
